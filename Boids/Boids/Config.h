a1e8589ced17089b07bf36aaaafccdda7f670dc7
Tobias G <31646748+Daikalos@users.noreply.github.com>
2022-07-26 13:33:58 +0200

    Update Config.h

--- Boids/Boids/Config.h
+++ Boids/Boids/Config.h
@@ -83,34 +83,36 @@ struct Config
 	static void load()
 	{
 		std::ifstream project_file("settings.json", std::ifstream::binary);
-		nlohmann::json json = nlohmann::json::parse(project_file);
 
-		if (!json.empty())
+		if (project_file.good())
 		{
-			try
+			nlohmann::json json = nlohmann::json::parse(project_file);
+			if (!json.empty())
 			{
-				background = convert(json["settings"]["background"]);
-
-				boid_count = json["settings"]["boid_count"];
-				boid_size_width = json["settings"]["boid_size_width"];
-				boid_size_height = json["settings"]["boid_size_height"];
-				boid_max_speed = json["settings"]["boid_max_speed"];
-				boid_min_speed = json["settings"]["boid_min_speed"];
-				boid_max_steer = json["settings"]["boid_max_steer"];
-				boid_view_angle = json["settings"]["boid_view_angle"];
-
-				sep_distance = json["settings"]["sep_distance"];
-				ali_distance = json["settings"]["ali_distance"];
-				coh_distance = json["settings"]["coh_distance"];
-
-				sep_weight = json["settings"]["sep_weight"];
-				ali_weight = json["settings"]["ali_weight"];
-				coh_weight = json["settings"]["coh_weight"];
-
-				color_option = json["settings"]["color_option"];
-
-				switch (Config::color_option)
+				try
 				{
+					background = convert(json["settings"]["background"]);
+
+					boid_count = json["settings"]["boid_count"];
+					boid_size_width = json["settings"]["boid_size_width"];
+					boid_size_height = json["settings"]["boid_size_height"];
+					boid_max_speed = json["settings"]["boid_max_speed"];
+					boid_min_speed = json["settings"]["boid_min_speed"];
+					boid_max_steer = json["settings"]["boid_max_steer"];
+					boid_view_angle = json["settings"]["boid_view_angle"];
+
+					sep_distance = json["settings"]["sep_distance"];
+					ali_distance = json["settings"]["ali_distance"];
+					coh_distance = json["settings"]["coh_distance"];
+
+					sep_weight = json["settings"]["sep_weight"];
+					ali_weight = json["settings"]["ali_weight"];
+					coh_weight = json["settings"]["coh_weight"];
+
+					color_option = json["settings"]["color_option"];
+
+					switch (Config::color_option)
+					{
 					case 0:
 					{
 						boid_color_top_left = convert(json["settings"]["boid_color_top_left"]);
@@ -144,63 +146,64 @@ struct Config
 							boid_cycle_colors[i] = convert(temp_colors[i]);
 					}
 					break;
-				}
-
-				{
-					std::vector<std::string> temp_processes = json["settings"]["audio_responsive_processes"];
-					audio_responsive_processes = std::vector<std::wstring>(temp_processes.size());
-
-					for (int i = 0; i < audio_responsive_processes.size(); ++i)
-					{
-						std::string process = temp_processes[i];
-						audio_responsive_processes[i] = std::wstring(process.begin(), process.end());
 					}
+
+					{
+						std::vector<std::string> temp_processes = json["settings"]["audio_responsive_processes"];
+						audio_responsive_processes = std::vector<std::wstring>(temp_processes.size());
+
+						for (int i = 0; i < audio_responsive_processes.size(); ++i)
+						{
+							std::string process = temp_processes[i];
+							audio_responsive_processes[i] = std::wstring(process.begin(), process.end());
+						}
+					}
+
+					audio_responsive_strength = json["settings"]["audio_responsive_strength"];
+					audio_responsive_limit = json["settings"]["audio_responsive_limit"];
+					audio_responsive_density = json["settings"]["audio_responsive_density"];
+
+					{
+						std::vector<std::string> temp_colors = json["settings"]["audio_responsive_colors"];
+						audio_responsive_colors = std::vector<sf::Vector3f>(temp_colors.size());
+
+						for (int i = 0; i < temp_colors.size(); ++i)
+							audio_responsive_colors[i] = convert(temp_colors[i]);
+					}
+
+					impulse_enabled = json["settings"]["impulse_enabled"];
+					impulse_size = json["settings"]["impulse_size"];
+					impulse_speed = json["settings"]["impulse_speed"];
+					impulse_fade_distance = json["settings"]["impulse_fade_distance"];
+
+					{
+						std::vector<std::string> temp_colors = json["settings"]["impulse_colors"];
+						impulse_colors = std::vector<sf::Vector3f>(temp_colors.size());
+
+						for (int i = 0; i < temp_colors.size(); ++i)
+							impulse_colors[i] = convert(temp_colors[i]);
+					}
+
+					gravity_enabled = json["settings"]["gravity_enabled"];
+					gravity_towards_factor = json["settings"]["gravity_towards_factor"];
+					gravity_away_factor = json["settings"]["gravity_away_factor"];
+
+					predator_enabled = json["settings"]["predator_enabled"];
+					predator_distance = json["settings"]["predator_distance"];
+					predator_factor = json["settings"]["predator_factor"];
+
+					turn_at_border = json["settings"]["turn_at_border"];
+					turn_margin_factor = json["settings"]["turn_margin_factor"];
+					turn_factor = json["settings"]["turn_factor"];
+
+					grid_extra_cells = json["settings"]["grid_extra_cells"];
+					camera_enabled = json["settings"]["camera_enabled"];
+					camera_zoom = json["settings"]["camera_zoom"];
+					vertical_sync = json["settings"]["vertical_sync"];
+					max_framerate = json["settings"]["max_framerate"];
 				}
-
-				audio_responsive_strength = json["settings"]["audio_responsive_strength"];
-				audio_responsive_limit = json["settings"]["audio_responsive_limit"];
-				audio_responsive_density = json["settings"]["audio_responsive_density"];
-
-				{
-					std::vector<std::string> temp_colors = json["settings"]["audio_responsive_colors"];
-					audio_responsive_colors = std::vector<sf::Vector3f>(temp_colors.size());
-
-					for (int i = 0; i < temp_colors.size(); ++i)
-						audio_responsive_colors[i] = convert(temp_colors[i]);
-				}
-
-				impulse_enabled = json["settings"]["impulse_enabled"];
-				impulse_size = json["settings"]["impulse_size"];
-				impulse_speed = json["settings"]["impulse_speed"];
-				impulse_fade_distance = json["settings"]["impulse_fade_distance"];
-
-				{
-					std::vector<std::string> temp_colors = json["settings"]["impulse_colors"];
-					impulse_colors = std::vector<sf::Vector3f>(temp_colors.size());
-
-					for (int i = 0; i < temp_colors.size(); ++i)
-						impulse_colors[i] = convert(temp_colors[i]);
-				}
-
-				gravity_enabled = json["settings"]["gravity_enabled"];
-				gravity_towards_factor = json["settings"]["gravity_towards_factor"];
-				gravity_away_factor = json["settings"]["gravity_away_factor"];
-
-				predator_enabled = json["settings"]["predator_enabled"];
-				predator_distance = json["settings"]["predator_distance"];
-				predator_factor = json["settings"]["predator_factor"];
-
-				turn_at_border = json["settings"]["turn_at_border"];
-				turn_margin_factor = json["settings"]["turn_margin_factor"];
-				turn_factor = json["settings"]["turn_factor"];
-
-				grid_extra_cells = json["settings"]["grid_extra_cells"];
-				camera_enabled = json["settings"]["camera_enabled"];
-				camera_zoom = json["settings"]["camera_zoom"];
-				vertical_sync = json["settings"]["vertical_sync"];
-				max_framerate = json["settings"]["max_framerate"];
+				catch (std::exception e) {}
 			}
-			catch (std::exception e) { }
 		}
 
 		min_distance = std::fmaxf(std::fmaxf(Config::sep_distance, Config::ali_distance), Config::coh_distance);
